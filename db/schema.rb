43dd818b8a760758c1bf1b83dc8f342367408c7a
toyo <toyokazuk@gmail.com>
2014-06-18 02:56:14 +0900

    change subtitle

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140525172734) do
+ActiveRecord::Schema.define(version: 20140527164527) do
+
+  create_table "mapusers", force: true do |t|
+    t.string   "title"
+    t.string   "description"
+    t.string   "address"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "microposts", force: true do |t|
     t.text     "content"
