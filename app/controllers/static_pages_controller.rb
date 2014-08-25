042cf141bd678a77c13357e5ab74e946af884391
x220 <toyokazuk@gmail.com>
2014-08-26 04:42:38 +0900

    画像ランダム表示

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,7 +5,8 @@ class StaticPagesController < ApplicationController
       @micropost  = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page])
     end
-    @time = Time.now.to_s
+    # @time = Time.now.to_s
+    @photos = ["/images/beer.jpg","/images/curry.jpg","/images/dou.jpg", "/images/ferry.jpg","/images/kindai.jpg", "/images/kuma.jpg","/images/tomoro.jpg","/images/mtrishiri.jpg"].shuffle
   end
 
   def help
